6d72446df4ccf7971b33ca900af659c635380f6b
dannxtrxn <78929874+dannxtrxn@users.noreply.github.com>
2021-03-02 10:57:20 -0500

    added thing to js

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,22 @@ function welcome(){
     let username = prompt("What is your name?");    
     let message = "Welcome " + username;
     document.getElementById("saywelcome").innerHTML = message; 
-
 }​​
+
+functions lights(){
+
+    let color = prompt("What color is the light now");
+
+    if (color == "red") {
+        console.log("STOP!")
+    }
+    else if (color == "green"){
+        console.log("GO!");
+    }
+    else if (color == "amber"){
+        console.log("Stop if it is safe to do so");
+    }
+    else {
+        console.log("invalid input");
+    }
+}
