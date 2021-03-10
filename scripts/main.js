a7fde37162fd5cd6596940d841d00a385f720b28
dannxtrxn <78929874+dannxtrxn@users.noreply.github.com>
2021-03-10 14:19:54 -0500

    edit my buttons

--- scripts/main.js
+++ scripts/main.js
@@ -11,5 +11,26 @@ function myFunction() {
  }
 
  function comingsoon() {
-    alert("This Assignment Will Be Coming Soon");}
+    alert("Assignment 1 Will Be Coming Soon");}
+
+    function comingsoon2() {
+    alert("Assignment 2 Will Be Coming Soon");}
+
+    function comingsoon3() {
+     alert("Assignment 3 Will Be Coming Soon");}
+
+    function comingsoon4() {
+    alert("Assignment 4 Will Be Coming Soon");}
+
+    function comingsoon5() {
+    alert("Assignment 5 Will Be Coming Soon");}
+
+    function comingsoon6() {
+    alert("Assignment 6 Will Be Coming Soon");}
+
+    function comingsoon7() {
+    alert("Assignment 7 Will Be Coming Soon");}
+
+    function comingsoon8() {
+    alert("Assignment 8 Will Be Coming Soon");}
 
